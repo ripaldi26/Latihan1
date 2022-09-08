2c78620beada8a3b1a1cfbeed383843b7ea3a13b
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-09-09 02:05:45 +0700

    Update LoopingC.c

--- LoopingC.c
+++ LoopingC.c
@@ -4,9 +4,9 @@ int main() {
 
 int i = 0;
   do{
-  printf( "Belajar Koding di Kelas Algoritma\n",i);
+  printf( "Belajar Koding di Kelas Algoritma \n");
     i++;
   }while(i < 5);
   
-   return 0;
+  return 0;
 }
