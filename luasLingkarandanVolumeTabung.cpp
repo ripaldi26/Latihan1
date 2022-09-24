3bdbb69cf667e8409527bf8e82ffa90c8caa08d2
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-09-25 03:22:57 +0700

    Create luasLingkarandanVolumeTabung.cpp

---
+++ luasLingkarandanVolumeTabung.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+# define phi 3.14152965
+using namespace std;
+int main()
+{
+
+float jari_jari, tinggi, volume, luas;
+jari_jari=5.0;
+tinggi=7;
+volume=phi*jari_jari*jari_jari*tinggi;
+luas=phi*jari_jari*jari_jari;
+cout << "volume tabung = "<<volume<<endl;
+cout <<"luas lingkaran = "<<luas<<endl;
+getchar();
+}
