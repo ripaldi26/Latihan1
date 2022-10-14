c0fc7c6d934de09a94a0cd26f2a3660541a1947b
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-10-14 17:42:39 +0700

    Create break.cpp

---
+++ break.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+  int a=3, b=2, c=1,bil;
+  printf("Bil-A\t| Bil-B | Bil-C\n");
+  printf("======================");
+  for(bil=1;bil<=10;++bil)
+  {
+    a+=b;b+=c,c+=2;
+    printf("\n%d\t\t|%d\t\t|%d",a,b,c);
+    if(c==13)
+      break;
+  }
+}
