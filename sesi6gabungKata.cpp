eaa5c2bc4ecb48b9c899bb0142ebc5a3346c3c77
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-10-28 15:56:25 +0700

    Update sesi6gabungKata.cpp

--- sesi6gabungKata.cpp
+++ sesi6gabungKata.cpp
@@ -1,14 +1,18 @@
+#include <ctype.h>
 #include <iostream>
-#include <string>
+#include <string.h>
 using namespace std;
-
 int main() {
-	
-	string yoi = "FAKULTAS PARIWISATA DAN TEKNOLOGI INFORMASI";
-	
-	cout<<" kata yang ingin dihitung adalah =  "<<yoi<<endl;
-	
-	
-	cout << " Panjang katanya adalah : " << yoi.length();
-	return 0;
+  char T[20] = "Teknik";
+  char I[20] = "Informatika";
+
+  cout << " kalimat pertama adalah = ";
+  cout << T;
+  cout << endl;
+  cout << " kalimat kedua adalah   = ";
+  cout << I;
+  cout << endl;
+  strcat(T, I);
+  cout << " Hasil Penggabungannya adalah = " << T;
+  return 0;
 }
