79ccd1ec2f4f088d31e475d136a1f5cc2d36a5bf
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-11-16 19:57:34 +0700

    Update tugasSesi8Matriks.cpp

--- tugasSesi8Matriks.cpp
+++ tugasSesi8Matriks.cpp
@@ -1,53 +1,68 @@
 #include <iostream>
 using namespace std;
-int main() {
-  int r, c, a[50][50], b[50][50], jumlah[50][50],kurang[50][50],kali[50][50], i, j;
-  cout << "Masukan Jumlah baris (1 - 50): ";
-  cin >> c;
-  cout << "Masukan Jumlah kolom (1 - 50): ";
-  cin >> r;
-  cout << endl << "Masukan matriks pertama: " << endl;
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      cout << "Matriks a" << i + 1  << j + 1  << " : ";
-      cin >> a[i][j];
-    }
-  cout << endl << "Masukan matriks kedua: " << endl;
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      cout << "Matriks b" << i + 1 << j + 1 << " : ";
 
-      cin >> b[i][j];
+int main() {
+  int mtrksA[50][50], mtrksB[50][50], result[50][50], p, q, r, s, t, u, v, jumlah = 0;
+  cout << "Masukkan jumlah baris matriks pertama(1 - 50) : ";
+  cin >> s;
+  cout << "Masukkan jumlah kolom matriks pertama(1 - 50) : ";
+  cin >> t;
+
+  cout << "Masukkan jumlah baris matriks kedua(1 - 50) : ";
+  cin >> u;
+  cout << "Masukkan jumlah kolom matriks kedua(1 - 50) : ";
+  cin >> v;
+
+  if (t != u) {
+    cout << "Matriks tidak dapat dikalikan satu sama lain ";
+  } else {
+
+    cout << "Masukkan Matriks Pertama : " << endl;
+    for (p = 0; p < s; p++) {
+      for (q = 0; q < t; q++) {
+        cout << "Matriks mtrksA" << p + 1 << q + 1 << " : ";
+        cin >> mtrksA[p][q];
+      }
     }
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j)
-      jumlah[i][j] = a[i][j] + b[i][j];
-  cout << endl << "Penjumlahan kedua matriks: " << endl;
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      cout << jumlah[i][j] << " ";
-      if (j == c - 1)
-        cout << endl;
+    cout << "\nMasukkan Matriks Kedua : " << endl;
+    for (p = 0; p < u; p++) {
+      for (q = 0; q < v; q++) {
+        cout << "Matriks mtrksB" << p + 1 << q + 1 << " : ";
+        cin >> mtrksB[p][q];
+      }
     }
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j)
-      kurang[i][j] = a[i][j] - b[i][j];
-  cout << endl << "Pengurangan kedua matriks: " << endl;
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      cout << kurang[i][j] << " ";
-      if (j == c - 1)
-        cout << endl;
+    cout << "\nHasil Penjumlahan Kedua Matriks : " << endl;
+    for (p = 0; p < s; p++) {
+      for (q = 0; q < t; q++) {
+        result[p][q] = mtrksA[p][q] + mtrksB[p][q];
+        cout << result[p][q] << "\t";
+      }
+      cout << endl;
     }
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j)
-      kali[i][j] = a[i][j] * b[i][j];
-  cout << endl << "Perkalian kedua matriks: " << endl;
-  for (i = 0; i < r; ++i)
-    for (j = 0; j < c; ++j) {
-      cout << kali[i][j] << " ";
-      if (j == c - 1)
-        cout << endl;
+    cout << "\nHasil Pengurangan Kedua Matriks : " << endl;
+    for (p = 0; p < s; p++) {
+      for (q = 0; q < t; q++) {
+        result[p][q] = mtrksA[p][q] - mtrksB[p][q];
+        cout << result[p][q] << "\t";
+      }
+      cout << endl;
     }
+    for (p = 0; p < s; p++) {
+      for (q = 0; q < v; q++) {
+        for (r = 0; r < u; r++) {
+          jumlah = jumlah + mtrksA[p][r] * mtrksB[r][q];
+        }
+        result[p][q] = jumlah;
+        jumlah = 0;
+      }
+    }
+    cout << "\nHasil Perkalian Kedua Matriks : " << endl;
+    for (p = 0; p < s; p++) {
+      for (q = 0; q < v; q++) {
+        cout << result[p][q] << "\t";
+      }
+      cout << endl;
+    }
+  }
   return 0;
 }
