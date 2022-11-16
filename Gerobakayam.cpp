801cad04d18763208afaf726e418b45f9a791117
ripaldi26 <113054922+ripaldi26@users.noreply.github.com>
2022-11-16 19:58:11 +0700

    Update Gerobakayam.cpp

--- Gerobakayam.cpp
+++ Gerobakayam.cpp
@@ -1,72 +1,58 @@
 #include <iostream>
 using namespace std;
+
 int main() {
-
-  int bj, bp;
-  float dada = 2500, paha = 2000, sayap = 1500;
+  int harga[10], x, y, u[10], jmlh[10], th, pjk;
   char jp;
-  cout << " GEROBAK FRIED CHICKEN " << endl;
-  cout << "-----------------------" << endl;
-  cout << " Kode\tJenis\tHarga" << endl;
-  cout << "-----------------------" << endl;
-  cout << "   D\tDADA\tRp. " << dada << endl;
-  cout << "   P\tPAHA\tRp. " << paha << endl;
-  cout << "   S\tSAYAP\tRp. " << sayap << endl;
-  cout << "-----------------------" << endl;
-  cout << " Banyak Jenis : ";
-  cin >> bj;
-  for (int a = 1; a <= bj; a++) {
-    cout << endl << endl;
-    cout << " Jenis ke : " << a << endl;
-    cout << " Jenis Potong [D/P/S] : ";
+  string tipe[10];
+  cout << "GEROBAK FRIED CHICKEN" << endl;
+  cout << "----------------------" << endl;
+  cout << "Kode\tJenis\tHarga" << endl;
+  cout << "----------------------" << endl;
+  cout << " D\tDada\t Rp.2500" << endl;
+  cout << " P\tPaha\t Rp.2000" << endl;
+  cout << " S\tSayap\t Rp.1500" << endl;
+  cout << "----------------------" << endl;
+  cout << "Banyak Jenis = ";
+  cin >> x;
+
+  for (y = 0; y < x; y++) {
+    cout << "Jenis ke - " << y + 1 << endl;
+  ulang:
+    cout << "Jenis Potong [D/P/S] : ";
     cin >> jp;
-    cout << " Banyak Potong : ";
-    cin >> bp;
-  }
-  cout << endl << endl << endl;
-  cout << "\t\t\t\t GEROBAK FRIED CHICKEN" << endl;
-  cout << "------------------------------------------------------" << endl;
-  cout << "No.\t\tJenis\t\t Harga\t\t Banyak\t\tJumlah" << endl;
-  cout << "   \t\tPotong\t\t Satuan\t\t Beli\t\tHarga" << endl;
-  cout << "------------------------------------------------------" << endl;
-  int jhsayap = sayap * bp, jhdada = dada * bp, jhpaha = paha * bp,
-      pjkdada = jhdada * 0.1, pjksayap = jhsayap * 0.1, pjkpaha = jhpaha * 0.1,
-      tbdada = jhdada + pjkdada, tbsayap = jhsayap + pjksayap,
-      tbpaha = jhpaha + pjkpaha;
-  for (int b = 1; b <= bj; b++) {
-    cout << b;
-    cout << "\t\t ";
-    if (jp == 'd' || jp == 'D') {
-      cout << "Dada\t\t Rp." << dada;
-      cout << "\t\t" << bp;
-      cout << "\t\tRp." << jhdada << endl;
-      cout << "------------------------------------------------------" << endl;
-      cout << "\t\t\t\t\tJumlah bayar : Rp. " << jhdada << endl;
-      cout << "\t\t\t\t\tPajak 10%    : Rp. " << pjkdada << endl;
-      cout << "\t\t\t\t\tTotal bayar  : Rp. " << tbdada << endl;
-      cout << "------------------------------------------------------" << endl;
-
-    } else if (jp == 'p' || jp == 'P') {
-      cout << "Paha\t\t Rp." << paha;
-      cout << "\t\t" << bp;
-      cout << "\t\tRp." << jhpaha << endl;
-      cout << "------------------------------------------------------" << endl;
-      cout << "\t\t\t\t\tJumlah bayar : Rp. " << jhpaha << endl;
-      cout << "\t\t\t\t\tPajak 10%    : Rp. " << pjkpaha << endl;
-      cout << "\t\t\t\t\tTotal bayar  : Rp. " << tbpaha << endl;
-      cout << "------------------------------------------------------" << endl;
-
-    } else if (jp == 's' || jp == 'S') {
-      cout << "Sayap\t\t Rp." << sayap;
-      cout << "\t\t" << bp;
-      cout << "\t\tRp." << jhsayap << endl;
-      cout << "------------------------------------------------------" << endl;
-      cout << "\t\t\t\t\tJumlah bayar : Rp. " << jhsayap << endl;
-      cout << "\t\t\t\t\tPajak 10%    : Rp. " << pjksayap << endl;
-      cout << "\t\t\t\t\tTotal bayar  : Rp. " << tbsayap << endl;
-      cout << "------------------------------------------------------" << endl;
+    if (jp == 'D' || jp == 'd') {
+      u[y] = 2500;
+      tipe[y] = "Dada";
+    } else if (jp == 'P' || jp == 'p') {
+      u[y] = 2000;
+      tipe[y] = "Paha";
+    } else if (jp == 'S' || jp == 's') {
+      u[y] = 3500;
+      tipe[y] = "Sayap";
+    } else {
+      cout << "Input invalid.\n";
+      goto ulang;
     }
+    cout << "Banyak Potong : ";
+    cin >> jmlh[y];
 
-    return 0;
+    harga[y] = jmlh[y] * u[y];
   }
+  cout << "\t\t GEROBAK FIRED CHICKEN " << endl;
+  cout << "-----------------------------------------" << endl;
+  cout << "No.\tJenis\t Harga    Banyak    Jumlah" << endl;
+  cout << "\tPotong \tSatuan    Beli      Harga " << endl;
+  cout << "-----------------------------------------" << endl;
+  for (y = 0; y < x; y++) {
+    cout << y + 1 << "   " << tipe[y] << "\t  " << u[y] << "     " << jmlh[y]
+         << "        Rp." << harga[y] << endl;
+    th += harga[y];
+  }
+  pjk = th * 0.1;
+  cout << "-----------------------------------------" << endl;
+  cout << "\t\t\t\t\t\tHarga total = " << th - 1<<endl;
+  cout << "\t\t\t\t\t\tPajak 10%   = " << pjk<<endl;
+  cout << "\t\t\t\t\t\tTotal Bayar = " << th + pjk - 1<<endl;
+  return 0;
 }
